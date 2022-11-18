8f2e3e25ca6a39fc8850107559e5c1e132a8f8e4
Christian Sousa <103544118+christiansousa93@users.noreply.github.com>
2022-11-18 08:49:00 -0300

    Delete 9.py

--- 9.py
+++
@@ -1,8 +0,0 @@
-NumA = int (input('Digite um número: '))
-NumB = int (input('Digite outro número: '))
-
-
-print ('Os valores digitados foram: ',NumA,NumB)
-print ('Os valores invertidos são: ',NumB,NumA)
-
-
