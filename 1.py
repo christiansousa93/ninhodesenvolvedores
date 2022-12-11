72a9ee04e42780f8f304166cf9a6df755cbd01d7
Christian Sousa <103544118+christiansousa93@users.noreply.github.com>
2022-12-11 08:21:10 -0300

    Delete 1.py

--- 1.py
+++
@@ -1,6 +0,0 @@
-a = int (input('Digite um número: '))
-b = int (input('Digite um número: '))
-c = int (input('Digite um número: '))
-
-if c >= (a + b):
-    print('A soma dos primeiros números é menor ou igual ao terceiro')
